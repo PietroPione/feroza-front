d7dceeca5e25d91b9e490cd1adae16ea4a2e28cc
PietroPione <pietrofrlavoro@gmail.com>
2025-02-28 16:12:42 +0100

    feat: traduzione componi piatto

--- app/menu/componi-piatto/page.jsx
+++ app/menu/componi-piatto/page.jsx
@@ -1,34 +1,60 @@
+"use client";
+
+import { LanguageContext } from "@/context/LanguageContext";
 import { createClient } from "@/prismicio";
 import PiattoMulti from "@/components/PiattoMulti";
 import ButtonPrimary from "@/components/buttonPrimary";
+import { useContext, useEffect, useState } from "react";
 
-export default async function ComponiPiatto() {
-    const client = createClient();
+export default function ComponiPiatto() {
+    const { language } = useContext(LanguageContext);
+    const [piattoMulti, setPiattoMulti] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    // Recupera l'infoPiattoMulti
-    const piattoMultiResponse = await client.getByType("piattomulti");
-    const piattoMulti = piattoMultiResponse.results[0];
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const client = createClient();
+            const piattoMultiResponse = await client.getByType("piattomulti", {
+                lang: language,
+            });
+            if (piattoMultiResponse?.results.length > 0) {
+                setPiattoMulti(piattoMultiResponse.results[0]);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [language]);
 
-    // Recupera tutte le slice di tipo "ripetibile_menu"
+    if (loading) {
+        return (
+            <div className="space-y-10 py-10 min-h-[500px]">
+                <div className="animate-pulse bg-white h-screen rounded"></div>
+            </div>
+        );
+    }
+
     const ripetibileMenuSlices = piattoMulti?.data.slices.filter(slice => slice.slice_type === "ripetibile_menu") || [];
     const titoliSezione = ripetibileMenuSlices.map(slice => slice.primary.titolosezione || "");
     const immaginiSezione = ripetibileMenuSlices.map(slice => slice.primary.immagine_sezione?.url || "");
     const traduzioniSezione = ripetibileMenuSlices.map(slice => slice.primary.traduzione_titolo || "");
     const elementiMenu = ripetibileMenuSlices.map(slice => slice.primary.elementi_menu || []);
 
-
-    // Dati per InfoPiattoMulti
     const piattoMultiSlice = piattoMulti?.data.slices.find(slice => slice.slice_type === "info_piatto_multi");
-    const infoPiattoMulti = piattoMultiSlice.primary?.infopiattomulti || [];
+    const infoPiattoMulti = piattoMultiSlice?.primary?.infopiattomulti || [];
+
+    const testoBottone = {
+        "it-it": "Torna al menu",
+        "en-us": "Back to menu",
+        // Aggiungi altre lingue se necessario
+    };
 
     return (
         <div className="space-y-10 py-10">
-
             {infoPiattoMulti && <PiattoMulti infoPiatti={infoPiattoMulti} titoliSezione={titoliSezione} traduzioniSezione={traduzioniSezione} immaginiSezione={immaginiSezione} elementiMenu={elementiMenu} />}
             <div className="text-center">
-                <ButtonPrimary url="/menu/" testo="Torna al menu" />
+                <ButtonPrimary url="/menu/" testo={testoBottone[language]} />
             </div>
-
         </div>
     );
 }
