3e270b2885cc2c9330457ef17fe6cfa0b54a77e3
PietroPione <pietrofrlavoro@gmail.com>
2025-02-28 16:41:03 +0100

    feat: traduzione cantina

--- app/cantina/page.jsx
+++ app/cantina/page.jsx
@@ -1,15 +1,42 @@
+"use client";
+
+import { LanguageContext } from "@/context/LanguageContext";
 import { createClient } from "@/prismicio";
 import CantinaClient from "@/components/CantinaClient";
+import { useContext, useEffect, useState } from "react";
 
-export default async function Cantina() {
-    const client = createClient();
-    const response = await client.getByType("cantina");
+export default function Cantina() {
+    const { language } = useContext(LanguageContext);
+    const [cantina, setCantina] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    if (!response.results.length) {
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const client = createClient();
+            const response = await client.getByType("cantina", {
+                lang: language,
+            });
+            if (response?.results.length > 0) {
+                setCantina(response.results[0]);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [language]);
+
+    if (loading) {
+        return (
+            <div className="space-y-10 py-10 min-h-[500px]">
+                <div className="animate-pulse bg-white h-screen rounded"></div>
+            </div>
+        );
+    }
+
+    if (!cantina) {
         return <div className="container">Nessuna cantina disponibile</div>;
     }
 
-    const cantina = response.results[0];
     const mappedSlices = cantina.data.slices.map((slice) => ({
         type: slice.slice_type,
         primary: slice.primary,
