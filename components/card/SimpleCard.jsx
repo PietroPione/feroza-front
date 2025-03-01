d5579dd4bab4efd64afd49c56e5e8e8734b88790
PietroPione <pietrofrlavoro@gmail.com>
2025-03-01 13:02:54 +0100

    fix: allineamento

--- components/card/SimpleCard.jsx
+++ components/card/SimpleCard.jsx
@@ -2,7 +2,7 @@ import ButtonPrimary from "@/components/buttonPrimary";
 
 export default function SimpleCard({ titolo, spiega, testoTasto, url, externalLink }) {
     return (
-        <div className="py-6 flex flex-col items-center justify-center md:items-start md:justify-start rounded-lg space-y-6">
+        <div className="py-6 flex flex-col items-start justify-start rounded-lg space-y-6">
             <div >
                 <h3 className="text-36 text-white font-semibold">{titolo}</h3>
                 <p className="text-white">{spiega}</p>
