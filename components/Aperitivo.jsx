dc3b4d82b23cd7f064401917b73855f8864e00e1
PietroPione <pietrofrlavoro@gmail.com>
2025-03-03 17:47:35 +0100

    fix: aperitivo

--- components/Aperitivo.jsx
+++ components/Aperitivo.jsx
@@ -19,32 +19,32 @@ export default function Aperitivo({
 
   return (
     <div className="relative" >
-      <div className="absolute top-0 left-[10vw]">
+      {/* <div className="absolute top-0 left-[10vw]">
         <img src={immagineTopSx} alt="" className="opacity-35 md:opacity-100 h-auto w-60" />
+      </div> */}
+      <div className="absolute top-[15vh] md:top-0 right-[10vw] md:right-[20vw]">
+        <img src={immagineTopDx} alt="" className="opacity-0 md:opacity-100 h-48 w-auto" />
       </div>
-      <div className="absolute top-[15vh] md:top-[10vh] right-[10vw] md:right-[25vw]">
-        <img src={immagineTopDx} alt="" className="opacity-35 md:opacity-100 h-24 w-auto" />
-      </div>
-      <div className="absolute bottom-[40vh] md:bottom-[20vh] md:left-[20vw]">
+      {/* <div className="absolute bottom-[40vh] md:bottom-[20vh] md:left-[20vw]">
         <img src={immagineBottomSx} alt="" className="opacity-35 md:opacity-100 h-36 w-auto" />
+      </div> */}
+      <div className="absolute bottom-[10vh] right-[10vw]">
+        <img src={immagineBottomDx} alt="" className="opacity-0 md:opacity-100 h-48 w-auto" />
       </div>
-      <div className="absolute bottom-[10vh] right-[20vw]">
-        <img src={immagineBottomDx} alt="" className="opacity-30 md:opacity-100 h-48 w-auto" />
-      </div>
 
 
-      <div className="container mx-auto p-4 space-y-4 md:space-y-6 text-center">
+      <div className="container mx-auto p-4 space-y-2 md:space-y-2 ">
 
-        <div className="space-y-2">
-          <h1 className="text-32 text-center font-bold">{titolo}</h1>
-          <h2 className="text-22 text-center">{sottotitolo}</h2>
+        <div className="space-y-2 md:marker:pt-2">
+          <h1 className="text-32  font-bold">{titolo}</h1>
+          <h2 className="text-22 ">{sottotitolo}</h2>
         </div>
 
         <ul className=" text-15 md:text-22 space-y-4">
           {nomeBevanda.map((bevanda, index) => (
-            <li key={index} className={`flex justify-center items-center gap-2s md:gap-10 ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}>
-              {bevanda.icona?.url && <img src={bevanda.icona.url} alt="" className="w-12 h-12" />}
-              <span>{bevanda.nome_bevanda}</span>
+            <li key={index} className={`flex justify-start items-start gap-2s md:gap-10 space-x-6`}>
+              <div>{bevanda.nome_bevanda}</div>
+              {bevanda.icona?.url && <img src={bevanda.icona.url} alt="" className="w-auto h-12" />}
             </li>
           ))}
         </ul>
@@ -53,11 +53,11 @@ export default function Aperitivo({
         <p className="text-17">{testoSalsa}</p>
         <ul className=" text-15 md:text-22">
           {salse.map((item, index) => (
-            <li key={index} className="flex items-center justify-center space-x-2 md:space-x-6">
+            <li key={index} className="flex items-start justify-start space-x-2 md:space-x-6">
               <div>
                 {item.nome_salsa}
               </div>
-              <img src={salseVegetariano} alt="" className="h-8 w-8" />
+              <img src={salseVegetariano} alt="" className="h-6 w-6" />
             </li>
           ))}
         </ul>
@@ -65,11 +65,11 @@ export default function Aperitivo({
 
         <ul className=" text-15 md:text-22">
           {falafel.map((item, index) => (
-            <li key={index} className="flex items-center justify-center space-x-2 md:space-x-6">
+            <li key={index} className="flex items-start justify-start space-x-2 md:space-x-6">
               <div>
                 {item.nome_falafel}
               </div>
-              <img src={falafelVegetariano} alt="" className="h-8 w-8" />
+              <img src={falafelVegetariano} alt="" className="h-6 w-6" />
             </li>
           ))}
         </ul>
